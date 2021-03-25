b1ffa29016b8a5a15481f79a51b1f472af3fee87
Ayesha <fxolbird@gmail.com>
2021-03-25 11:36:03 -0700

    changes

---
+++ PAckage.java
@@ -0,0 +1,23 @@
+package region;
+
+public class PAckage {
+    public static void main(String []args) {
+        for ( int b=0; b<args.length; b++)
+        System.out.println(args[b]);  
+       }
+    } 
+  
+
+
+
+
+
+// package region;
+
+// public class PAckage {
+//     public static void main(String []args) {
+//         for ( int b=0; b<args.length; b++)
+//         System.out.println(args[b]);  
+//        }
+//     } 
+  
